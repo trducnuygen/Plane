1d56e41f2ffcdad208b1e5d61084608483a6abbc
Duc Nguyen <nguyenminhduc890@gmail.com>
2022-07-12 21:53:37 +0700

    Create 6-9-2022.py

---
+++ 6-9-2022.py
@@ -0,0 +1,39 @@
+import random
+import time
+import  sys
+from  Adafruit_IO import  MQTTClient
+
+AIO_FEED_ID = ""
+AIO_USERNAME = "namelessbtw"
+AIO_KEY = "aio_QMsK29fJHPQvTTmWofsI26snFeSA"
+
+def  connected(client):
+    print("Service connected")
+    client.subscribe(AIO_FEED_ID)
+
+def  subscribe(client , userdata , mid , granted_qos):
+    print("Subscribed")
+
+def  disconnected(client):
+    print("Disconnected!!!")
+    sys.exit (1)
+
+def  message(client , feed_id , payload):
+    print("Data received " + payload)
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+
+while True:
+    value = random.randint(30, 60)    //range -90, 90 lattitude
+    print("Update humidity:", value)
+    client.publish("humidity", value)       // -180, 180
+    value1 = random.randint(0, 50)
+    print("Update temperature:", value1)
+    client.publish("temperature",value1)
+    time.sleep(10)
